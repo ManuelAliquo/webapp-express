908fcfaca49712dba4db557c3ffd1901f7f85da1
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-03-16 20:07:50 +0100

    add router

--- app.js
+++ app.js
@@ -5,68 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-// db connection
-const connection = require("./db/connection.js");
+// router
+const moviesRouter = require("./routers/moviesRouter.js");
 
-//* ROUTES
-
-// index
-app.get("/", (req, res) => {
-  const moviesSQL = "SELECT * FROM movies;";
-
-  connection.query(moviesSQL, (err, results) => {
-    if (err)
-      return res.status(500).json({
-        success: false,
-        message: "Database query failed",
-      });
-
-    res.status(200).json({
-      success: true,
-      result: results,
-    });
-  });
-});
-
-// show
-app.get("/:id", (req, res) => {
-  const { id } = req.params;
-
-  const moviesSQL = "SELECT * FROM movies WHERE id = ?";
-
-  connection.query(moviesSQL, [id], (err, movieResults) => {
-    if (err)
-      return res.status(500).json({
-        success: false,
-        result: "Database query failed",
-      });
-
-    const [movie] = movieResults;
-
-    if (!movie)
-      return res.status(404).json({
-        success: false,
-        result: "Resource not found",
-      });
-
-    const reviewsSQL = "SELECT * FROM reviews WHERE movie_id = ?";
-
-    connection.query(reviewsSQL, [id], (err, reviewResults) => {
-      if (err)
-        return res.status(500).json({
-          success: false,
-          result: "Database query failed",
-        });
-
-      movie.reviews = reviewResults;
-
-      res.status(200).json({
-        success: true,
-        result: movie,
-      });
-    });
-  });
-});
+app.use("/movies", moviesRouter);
 
 app.listen(process.env.APP_PORT, () =>
   console.log(`Server listening on ${process.env.APP_URL}:${process.env.APP_PORT}`),
