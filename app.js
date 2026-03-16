5678dffeab538dad2119481ac5de5c41e16993fd
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-03-16 15:50:18 +0100

    add connection to db and express middlewares

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+const connection = require("./db/connection.js");
+
+app.listen(3000, () => {
+  console.log("Server is listening");
+});
