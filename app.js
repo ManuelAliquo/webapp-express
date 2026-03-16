a79ff3b4859c5045be44d4a58354b8f681540b9e
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-03-16 17:04:50 +0100

    add show route

--- app.js
+++ app.js
@@ -11,11 +11,33 @@ app.use(express.static("public"));
 // db connection
 const connection = require("./db/connection.js");
 
-// routes
+//* routes
+
+// index
 app.get("/", (req, res) => {
-  const moviesSQL = "SELECT * FROM movies_db.movies;";
+  const moviesSQL = "SELECT * FROM movies;";
 
   connection.query(moviesSQL, (err, results) => {
+    if (err)
+      return res.status(500).json({
+        success: false,
+        message: "Database query failed",
+      });
+
+    res.status(200).json({
+      success: true,
+      result: results,
+    });
+  });
+});
+
+// show
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const moviesSQL = "SELECT * FROM movies WHERE id = ?;";
+
+  connection.query(moviesSQL, [id], (err, results) => {
     if (err)
       return res.status(500).json({
         success: false,
